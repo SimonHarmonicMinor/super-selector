0eec083bcad16e1def3f60f03cfacaa954198eff
Simon Kirekov <kirekov@gmail.com>
2020-07-22 15:14:56 +0700

    Added CachedResult class for storing cached values

---
+++ src/main/kotlin/com/github/simonharmonicminor/super_selector/Utils.kt
@@ -0,0 +1,12 @@
+package com.github.simonharmonicminor.super_selector
+
+class CachedResult<T>(private val supplier: () -> T) {
+    private val cache = mutableListOf<T>()
+
+    operator fun invoke(): T {
+        if (cache.isEmpty()) {
+            cache.add(supplier())
+        }
+        return cache[0]
+    }
+}
