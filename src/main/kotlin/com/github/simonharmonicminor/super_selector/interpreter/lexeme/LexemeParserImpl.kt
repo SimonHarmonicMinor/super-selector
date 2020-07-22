22c2daf5a9717a68fb5bf13dba0bcef0e824ffb2
Simon Kirekov <kirekov@gmail.com>
2020-07-22 18:26:42 +0700

    LexemeParserImpl now able to parse field names

--- src/main/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeParserImpl.kt
+++ src/main/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeParserImpl.kt
@@ -1,6 +1,18 @@
 package com.github.simonharmonicminor.super_selector.interpreter.lexeme
 
 import com.github.simonharmonicminor.super_selector.CachedResult
+import com.github.simonharmonicminor.super_selector.LexemeParsingException
+import java.lang.StringBuilder
+
+internal fun String.takeWhileIndexed(predicate: (Int, Char) -> Boolean): String {
+    val builder = StringBuilder()
+    for (i in this.indices) {
+        if (!predicate(i, this[i]))
+            break
+        builder.append(this[i])
+    }
+    return builder.toString()
+}
 
 internal class LexemeParserImpl : LexemeParser {
     private val query: String
@@ -18,7 +30,8 @@ internal class LexemeParserImpl : LexemeParser {
     }
 
     override fun peekNextLexeme(): Lexeme? {
-        return nextLexemeRetriever().first
+        val (lexeme, _) = nextLexemeRetriever()
+        return lexeme
     }
 
     override fun movedToNextLexeme(): LexemeParser {
@@ -29,6 +42,34 @@ internal class LexemeParserImpl : LexemeParser {
     private fun getNextLexeme(): Pair<Lexeme?, Int> {
         if (index >= query.length)
             return Pair(null, index)
-        return Pair(null, index)
+        var currIndex = index
+        while (currIndex < query.length) {
+            val currChar = query[currIndex]
+            if (IGNORED_SYMBOLS.contains(currChar)) {
+                currIndex++
+                continue
+            }
+            if (query[currIndex].isLetter()) {
+                val identifier = collectIdentifier(currIndex)
+                return Pair(Lexeme.of(LexemeType.FIELD, identifier), currIndex + identifier.length)
+            }
+            throw LexemeParsingException("Unexpected character at position ${currIndex + 1}: '${currChar}'")
+        }
+        return Pair(null, currIndex)
     }
- }
+
+    private fun collectIdentifier(startIndex: Int) =
+        collectCharsWhileConditionTrue(startIndex) { index, ch ->
+            if (index == startIndex)
+                ch.isLetter()
+            ch.isLetter() || ch.isDigit()
+        }
+
+    private fun collectCharsWhileConditionTrue(startIndex: Int, condition: (Int, Char) -> Boolean): String {
+        return query.substring(startIndex).takeWhileIndexed(condition)
+    }
+
+    companion object {
+        private val IGNORED_SYMBOLS = setOf(' ', '\t', '\n', '\r')
+    }
+}
--- src/test/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeParserImplTest.kt
+++ src/test/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeParserImplTest.kt
@@ -7,19 +7,31 @@ import org.junit.jupiter.api.Assertions.*
 internal class LexemeParserImplTest {
 
     @Test
-    fun peekNextLexeme() {
-        val parser = LexemeParser.of("asdasd")
-        assertNull(parser.peekNextLexeme())
+    fun parseFieldNames() {
+        val parser = LexemeParser.of("one two three3 four")
+
+        val lexeme1 = parser.peekNextLexeme()
+        val lexeme2 = parser.movedToNextLexeme()
+            .peekNextLexeme()
+        val lexeme3 = parser.movedToNextLexeme()
+            .movedToNextLexeme()
+            .peekNextLexeme()
+        val lexeme4 = parser.movedToNextLexeme()
+            .movedToNextLexeme()
+            .movedToNextLexeme()
+            .peekNextLexeme()
+
+        assertEquals(LexemeType.FIELD, lexeme1?.lexemeType)
+        assertEquals("one", lexeme1?.value)
+
+        assertEquals(LexemeType.FIELD, lexeme2?.lexemeType)
+        assertEquals("two", lexeme2?.value)
+
+        assertEquals(LexemeType.FIELD, lexeme3?.lexemeType)
+        assertEquals("three3", lexeme3?.value)
+
+        assertEquals(LexemeType.FIELD, lexeme4?.lexemeType)
+        assertEquals("four", lexeme4?.value)
     }
 
-    @Test
-    fun movedToNextLexeme() {
-        val parser = LexemeParser.of("dasdasd")
-        assertNull(
-            parser.movedToNextLexeme()
-                .movedToNextLexeme()
-                .movedToNextLexeme()
-                .peekNextLexeme()
-        )
-    }
 }
