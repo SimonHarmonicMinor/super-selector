6a89e2cde06adf452a217bc1dec598b4af49a9f3
Simon Kirekov <kirekov@gmail.com>
2020-07-26 20:04:31 +0700

    Added negative numbers support

--- src/test/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeMachineImplTest.kt
+++ src/test/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeMachineImplTest.kt
@@ -1,10 +1,9 @@
 package com.github.simonharmonicminor.super_selector.interpreter.lexeme
 
 import com.github.simonharmonicminor.super_selector.LexemeParsingException
-import com.github.simonharmonicminor.super_selector.interpreter.Pointer
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -73,7 +72,7 @@ internal class LexemeMachineImplTest {
 
     @Test
     fun parseDecimalNumbers() {
-        var parser = LexemeMachine.of("10 231 199   \n 445")
+        var parser = LexemeMachine.of("10 231 -199   \n 445")
 
         val lexeme1 = parser.peekNextLexeme().also { parser = parser.movedToNextLexeme() }
         val lexeme2 = parser.peekNextLexeme().also { parser = parser.movedToNextLexeme() }
@@ -87,7 +86,7 @@ internal class LexemeMachineImplTest {
         assertEquals(231L, lexeme2.value)
 
         assertEquals(LexemeType.DECIMAL, lexeme3.lexemeType)
-        assertEquals(199L, lexeme3.value)
+        assertEquals(-199L, lexeme3.value)
 
         assertEquals(LexemeType.DECIMAL, lexeme4.lexemeType)
         assertEquals(445L, lexeme4.value)
@@ -95,7 +94,7 @@ internal class LexemeMachineImplTest {
 
     @Test
     fun parseDoubleNumbers() {
-        var parser = LexemeMachine.of("10.1 56.2 78.004")
+        var parser = LexemeMachine.of("10.1 56.2 -78.004")
 
         val lexeme1 = parser.peekNextLexeme().also { parser = parser.movedToNextLexeme() }
         val lexeme2 = parser.peekNextLexeme().also { parser = parser.movedToNextLexeme() }
@@ -108,7 +107,7 @@ internal class LexemeMachineImplTest {
         assertEquals(56.2, lexeme2.value)
 
         assertEquals(LexemeType.DOUBLE, lexeme3.lexemeType)
-        assertEquals(78.004, lexeme3.value)
+        assertEquals(-78.004, lexeme3.value)
     }
 
     @Test
