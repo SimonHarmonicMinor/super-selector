c7cb364872b137b141600b34b25c8aba8d48d0f2
Simon Kirekov <kirekov@gmail.com>
2020-08-03 20:47:31 +0700

    Refactored LexemeParsersContainer. Now it implements LexemeParser.kt

--- src/main/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/handler/LexemeParsersContainer.kt
+++ src/main/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/handler/LexemeParsersContainer.kt
@@ -1,27 +1,20 @@
 package com.github.simonharmonicminor.super_selector.interpreter.lexeme.handler
 
-import com.github.simonharmonicminor.super_selector.LexemeParsingException
 import com.github.simonharmonicminor.super_selector.interpreter.lexeme.LexemeParsingResult
 import com.github.simonharmonicminor.super_selector.interpreter.lexeme.QueryState
 
-class LexemeParsersContainer(private vararg val handlers: LexemeParser) {
-    fun parseLexeme(queryState: QueryState): LexemeParsingResult {
+class LexemeParsersContainer(private vararg val parsers: LexemeParser) : LexemeParser {
+    override fun parseLexeme(queryState: QueryState): LexemeParsingResult? {
         var localQueryState = queryState
         while (IGNORED_SYMBOLS.contains(localQueryState.currentChar)) {
             localQueryState = localQueryState.nextCharState()
         }
-        for (handler in handlers) {
-            val result = handler.parseLexeme(localQueryState)
+        for (parser in parsers) {
+            val result = parser.parseLexeme(localQueryState)
             if (result != null)
                 return result
         }
-        val message = localQueryState.currentChar?.let {
-            "Unexpected character: $it"
-        } ?: "Reached the end of the query. No more lexemes are available"
-        throw LexemeParsingException(
-            localQueryState,
-            message
-        )
+        return null
     }
 
     companion object {
