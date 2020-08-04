a3bbf36f62fa6c9be1a2dc99ae16ca944cb7a5e5
Simon Kirekov <kirekov@gmail.com>
2020-08-04 22:29:20 +0700

    Refactored CachedResultTest

--- src/test/kotlin/com/github/simonharmonicminor/super_selector/CachedResultTest.kt
+++ src/test/kotlin/com/github/simonharmonicminor/super_selector/CachedResultTest.kt
@@ -1,28 +1,26 @@
 package com.github.simonharmonicminor.super_selector
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import org.mockito.Mockito.*
 
 internal class CachedResultTest {
 
     @Test
     fun invoke() {
-        val mockObj = mock(Any::class.java)
         val func = object : InvokeFunc<Int> {
+            private var counter = 1
+
             override fun get(): Int {
-                return 123
+                return counter++
             }
         }
 
         val cachedResult = CachedResult(func::get)
 
-        assertEquals(123, cachedResult())
-        assertEquals(123, cachedResult())
-        assertEquals(123, cachedResult())
-        assertEquals(123, cachedResult())
-        verify(mockObj, times(1)).hashCode()
+        assertEquals(1, cachedResult())
+        assertEquals(1, cachedResult())
+        assertEquals(1, cachedResult())
+        assertEquals(1, cachedResult())
     }
 
     private interface InvokeFunc<T> {
