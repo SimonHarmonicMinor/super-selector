0fb136527e0902163b9f99829196ea52f598419d
Simon Kirekov <kirekov@gmail.com>
2020-08-06 16:42:07 +0700

    Added integration test for LexemeMachineImpl to determine comparing operators

--- src/test/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeMachineIntegrationTest.kt
+++ src/test/kotlin/com/github/simonharmonicminor/super_selector/interpreter/lexeme/LexemeMachineIntegrationTest.kt
@@ -1,8 +1,10 @@
 package com.github.simonharmonicminor.super_selector.interpreter.lexeme
 
+import com.github.simonharmonicminor.super_selector.LexemeParsingException
 import com.github.simonharmonicminor.super_selector.interpreter.Pointer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class LexemeMachineIntegrationTest {
     @Test
@@ -24,5 +26,44 @@ class LexemeMachineIntegrationTest {
 
         assertEquals(LexemeType.LEFT_SQUARE_BRACKET, leftSquareBracket.lexemeType)
         assertEquals(Pointer(1, 6), leftSquareBracket.pointer)
+
+        assertThrows<LexemeParsingException> { lexemeMachine.peekNextLexeme() }
+    }
+
+    @Test
+    fun parseComparingOperators() {
+        var lexemeMachine = LexemeMachine.of("> \n   >= \n \n  < <= \n = !! !=")
+        val gt = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val ge = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val lt = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val le = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val eq = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val deny1 = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val deny2 = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+        val notEq = lexemeMachine.peekNextLexeme().also { lexemeMachine = lexemeMachine.movedToNextLexeme() }
+
+        assertEquals(LexemeType.GT, gt.lexemeType)
+        assertEquals(Pointer(0, 0), gt.pointer)
+
+        assertEquals(LexemeType.GE, ge.lexemeType)
+        assertEquals(Pointer(1, 3), ge.pointer)
+
+        assertEquals(LexemeType.LT, lt.lexemeType)
+        assertEquals(Pointer(3, 2), lt.pointer)
+
+        assertEquals(LexemeType.LE, le.lexemeType)
+        assertEquals(Pointer(3, 4), le.pointer)
+
+        assertEquals(LexemeType.EQ, eq.lexemeType)
+        assertEquals(Pointer(4, 1), eq.pointer)
+
+        assertEquals(LexemeType.DENY, deny1.lexemeType)
+        assertEquals(Pointer(4, 3), deny1.pointer)
+
+        assertEquals(LexemeType.DENY, deny2.lexemeType)
+        assertEquals(Pointer(4, 4), deny2.pointer)
+
+        assertEquals(LexemeType.NOT_EQ, notEq.lexemeType)
+        assertEquals(Pointer(4, 6), notEq.pointer)
     }
 }
